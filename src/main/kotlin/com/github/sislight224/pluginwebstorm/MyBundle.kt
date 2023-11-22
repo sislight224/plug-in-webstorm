c5633cef70a630f9dc6c3e95520d37f10e48699a
GitHub Action <action@github.com>
2023-11-22 17:38:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/sislight224/pluginwebstorm/MyBundle.kt
+++ src/main/kotlin/com/github/sislight224/pluginwebstorm/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sislight224.pluginwebstorm
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/sislight224/pluginwebstorm/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/sislight224/pluginwebstorm/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.sislight224.pluginwebstorm.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
